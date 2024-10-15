b427fac4926c8082f7f7262711d83a3293a09204
hubteas <tykim888@naver.com>
2024-10-15 13:26:11 +0900

    20241015 김태유 쪽지함 수정중3

--- publishing/js/message/mailBox.js
+++ publishing/js/message/mailBox.js
@@ -1,26 +1,17 @@
+//헤더푸터
+$(function () {
+$("#header").load("../main/header.html");
+});
 
-//휴지통 이동 기능(참고용)
-// document.getElementById('deleteButton').addEventListener('click', function() {
-//   const messageList = document.getElementById('messageList');
-//   const trashList = document.getElementById('trashList');
-  
-//   // 받은 쪽지 중 체크된 항목을 찾아서 휴지통으로 이동
-//   const checkboxes = document.querySelectorAll('.message-checkbox');
-//   checkboxes.forEach((checkbox, index) => {
-//       if (checkbox.checked) {
-//           const messageItem = checkbox.parentElement;
-//           messageList.removeChild(messageItem); // 받은 쪽지함에서 삭제
-//           trashList.appendChild(messageItem);   // 휴지통으로 이동
-//           checkbox.checked = false; // 체크 상태 초기화
-//       }
-//   });
-// });
+$(function () {
+$("#footer").load("../main/footer.html");
+});
 
 // 전체클릭 기능
 let checkAll = document.querySelector('.all');
 let checkItem = document.querySelectorAll('.item');
-console.log(checkAll);
-console.log(checkItem);
+// console.log(checkAll);
+// console.log(checkItem);
 
 checkAll.addEventListener('click', function() {
   checkItem.forEach(function(e) {
@@ -40,3 +31,74 @@ checkItem.forEach(function(e) {
       }
   });
 });
+
+//휴지통 이동
+// 왜...하나만제거가 되는거냐.....
+const btnDelete = document.querySelector('.btn-delete');
+console.log(btnDelete);
+const mailboxList = document.querySelectorAll('.mailbox-container');
+console.log(mailboxList);
+
+btnDelete.addEventListener('click', function() {
+  // const mailboxList = document.querySelector('mailbox-list');
+  // console.log(mailboxList);
+  // const trashList = document.getElementById('trashList');
+  
+  // 받은 쪽지 중 체크된 항목을 찾아서 휴지통으로 이동
+  const checkboxes = document.querySelectorAll('.item');
+  // console.log(checkboxes);
+  checkboxes.forEach((checkbox, index) => {
+      if (checkbox.checked) {
+          // const messageItem = checkbox.parentElement;
+          // console.log(messageItem);
+          // const msgDelList= messageItem.parentElement;
+          // console.log(msgDelList);
+          const messageItem = checkbox.closest('.mailbox-list');
+          mailboxList.forEach(list => {
+            list.removeChild(messageItem);
+          }); // 받은 쪽지함에서 삭제
+          // trashList.appendChild(messageItem);   // 휴지통으로 이동
+          checkbox.checked = false; // 체크 상태 초기화
+      }
+  });
+});
+
+//쪽지클릭시 쪽지내용보기
+let mailContent = document.querySelectorAll('.mail-content');
+mailContent.forEach(btn => {
+  btn.addEventListener("click", function(){
+    let getMsgContainer = document.querySelector('.getmsg-container');
+    getMsgContainer.style.display="block";
+  })
+});
+
+let replyMsg = document.querySelector('.reply-msg');
+replyMsg.addEventListener("click", function(){
+    let sendMsgContainer = document.querySelector('.sendmsg-container');
+    sendMsgContainer.style.display="block";
+  });
+
+
+//쪽지 닫기 기능
+// let btnClose = document.querySelector('.btn-close');
+// console.log(btnClose);
+
+// btnClose.addEventListener("click", function(){
+//   const closeItem = btnClose.closest('.nonmodal-container');
+//   // let nonmodalContainer = document.querySelector('.nonmodal-container');
+//   // console.log(nonmodalContainer);
+//   closeItem.style.display = 'none';
+// })
+// 아 이건 하나만제거되고;;
+// 아래건 전부제거가되네........난감하다
+let btnClose = document.querySelectorAll('.btn-close');
+btnClose.forEach(btn => {
+  btn.addEventListener("click", function() {
+    let nonmodalContainers = document.querySelectorAll('.nonmodal-container');
+    
+    nonmodalContainers.forEach(container => {
+      container.style.display = 'none';
+    });
+  });
+});
+
