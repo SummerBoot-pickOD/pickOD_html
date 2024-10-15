351a8cb3d7a55801337f41020883fdfd53243866
yujean-c <yujeanc.258@gmail.com>
2024-10-16 06:17:34 +0900

    20241016 06:00 최유진 admReplies js에 모달 닫기 이벤트, 헤더추가 이외 모든 부분 완료

--- publishing/js/admin/admReplies.js
+++ publishing/js/admin/admReplies.js
@@ -2,6 +2,28 @@ $(function () {
   $("#header").load("../../html/main/header.html");
 });
 
+
 $(function () {
   $("#footer").load("../../html/main/footer.html");
+});
+
+
+//상세보기 누르면 댓글 상세보기 모달 띄우기
+const openModal = document.querySelectorAll('.detail-button');
+const cmtDetailModal = document.querySelector('.nonmodal-container');
+const closeBtn = document.querySelector('.close-btn');
+
+
+openModal.forEach(btn => {
+  addEventListener("click", function () {
+    // console.log("clicked")
+    cmtDetailModal.style.display = "flex";
+  })
+});
+
+
+// x 누르면 모달 닫기 -> 되다가 다시 안되기 시작. 콘솔 출력은 됨. 수정필요
+closeBtn.addEventListener("click", function () {
+  console.log("모달 닫기 버튼 ");
+  cmtDetailModal.style.display = "none";
 });
