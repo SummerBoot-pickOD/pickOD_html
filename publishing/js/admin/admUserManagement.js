e8ae652ca08dd6a0b9c8a6baffd0f9d55137049c
yujean-c <yujeanc.258@gmail.com>
2024-10-17 05:01:52 +0900

    20241017 05:00 최유진 admUserManagement html, css, js 수정하여 모든 버튼에 이벤트/링크 추가 및 쪽지보내기 모달 추가 완료

--- publishing/js/admin/admUserManagement.js
+++ publishing/js/admin/admUserManagement.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
     $("#footer").load("../../html/main/footer.html");
   });
 
-  //상세보기 누르면 댓글 상세보기 모달 띄우기
+  //댓글 상세보기 모달 
   const openModal = document.querySelectorAll('.user-detail');
   const usrDetailModal = document.querySelector('.nonmodal-container');
   const closeBtn = document.querySelector('.close-btn');
 
-
+  //댓글 상세보기 모달 띄우기
   openModal.forEach(btn => {
     btn.addEventListener("click", function () {
       // console.log("clicked")
@@ -21,9 +21,34 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
-  // x 누르면 모달 닫기 -> 되다가 다시 안되기 시작. 콘솔 출력은 됨. 수정필요
+  //댓글 상세보기 모달 닫기
   closeBtn.addEventListener("click", function () {
-    console.log("모달 닫기 버튼 ");
     usrDetailModal.style.display = "none";
   });
+
+
+  //쪽지 보내기 상세보기 모달 
+  const openMessage = document.querySelector('.message');
+  const msgModal = document.querySelector('.sendmsg-container');
+  const closeModal = document.querySelector('.message-close');
+  const sendMsg = document.querySelector(".send-msg")
+
+  //쪽지 보내기 모달 띄우기
+  openMessage.addEventListener("click", function () {
+    msgModal.style.display = "flex";
+  })
+
+  //쪽지 보내기 모달 닫기
+  closeModal.addEventListener("click", function () {
+    msgModal.style.display = "none";
+  });
+
+  //쪽지 보내기 버튼 클릭 시 
+  sendMsg.addEventListener("click", function () {
+    alert("쪽지를 전송하였습니다.");
+    msgModal.style.display = "none";
+    // 쪽지 내용 전달됨 
+  })
 });
+
+
