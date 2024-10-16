ec8bcdb1de3305c1015700910cb37dd16671bee7
yujean-c <yujeanc.258@gmail.com>
2024-10-16 21:36:24 +0900

    20241016 21:36 최유진 템플릿 상세보기 페이지 html, css, js 1차 작성

---
+++ publishing/js/admin/admTemplateDetails.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    $("#header").load("../../html/admin/admHeader.html");
+  });
+
+  $(function () {
+    $("#footer").load("../../html/main/footer.html");
+  });
+});
