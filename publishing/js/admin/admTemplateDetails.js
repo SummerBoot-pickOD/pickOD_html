438302377a2b7b18215e163f4986d2798eafaf23
yujean-c <yujeanc.258@gmail.com>
2024-10-17 05:59:51 +0900

    20241017 05:58 최유진 관리자템플릿상세페이지 일반유저 상세페이지 기준 관리자용 버튼2개 추가 및 css 수정

--- publishing/js/admin/admTemplateDetails.js
+++ publishing/js/admin/admTemplateDetails.js
@@ -6,4 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
   $(function () {
     $("#footer").load("../../html/main/footer.html");
   });
+
+  //템플릿 수정하기 버튼 
+  const toTemplate = document.querySelector(".to-template");
+  toTemplate.addEventListener("click", function () {
+    window.location.href = "../../html/admCreateTemplate.html";
+  })
+
+  //댓글보기 버튼 
+  const toComment = document.querySelector(".to-comment");
+  toComment.addEventListener("click", function () {
+    window.location.href = "../../html/admin/admReplies.html";
+  })
 });
