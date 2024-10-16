2c35ecba6f8b6f54a4f2bffe1676d882077f6c93
yujean-c <yujeanc.258@gmail.com>
2024-10-17 05:12:35 +0900

    20241017 05:12 최유진 템플릿 관리 페이지 admTemplate 버튼 클릭 이벤트 추가 완료

--- publishing/js/admin/admTemplate.js
+++ publishing/js/admin/admTemplate.js
@@ -1,7 +1,20 @@
-$(function () {
-  $("#header").load("../../html/admin/admHeader.html");
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    $("#header").load("../../html/admin/admHeader.html");
+  });
+
+  $(function () {
+    $("#footer").load("../../html/main/footer.html");
+  });
+
+  //템플릿 상세보기 > 템플릿 상세 페이지로 이동
+  const toTemplate = document.querySelectorAll('.to-template');
+
+  toTemplate.forEach(btn => {
+    btn.addEventListener("click", function () {
+      window.location.href = "../../html/admin/admTemplateDetails.html";
+    })
+  });
 });
 
-$(function () {
-  $("#footer").load("../../html/main/footer.html");
-});
+
