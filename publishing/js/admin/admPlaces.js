2e556d998de21f52524dec4dddf567bca67df61b
yujean-c <yujeanc.258@gmail.com>
2024-10-17 01:32:09 +0900

    20241017 01:31 최유진 장소관리페이지 admPlaces  버튼에 링크 추가, 주소 부분 클릭 시 펼치기 기능 추가

--- publishing/js/admin/admPlaces.js
+++ publishing/js/admin/admPlaces.js
@@ -1,7 +1,33 @@
-$(function () {
-  $("#header").load("../../html/admin/admHeader.html");
-});
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    $("#header").load("../../html/admin/admHeader.html");
+  });
 
-$(function () {
-  $("#footer").load("../../html/main/footer.html");
+  $(function () {
+    $("#footer").load("../../html/main/footer.html");
+  });
+
+
+  const openCmt = document.querySelectorAll('.to-comments');
+  const toPlace = document.querySelectorAll('.to-place');
+
+  openCmt.forEach(btn => {
+    btn.addEventListener("click", function () {
+      window.location.href = "../../html/admin/admReplies.html";
+    });
+  });
+
+  toPlace.forEach(btn => {
+    btn.addEventListener("click", function () {
+      window.location.href = "../../admin//place/admplaceDetails.html";
+    });
+  });
+
+  const showAdd = document.querySelectorAll(".address");
+  showAdd.forEach(cell => {
+    cell.addEventListener('click', function () {
+      console.log("클릭됨")
+      this.classList.toggle("expanded");
+    })
+  })
 });
