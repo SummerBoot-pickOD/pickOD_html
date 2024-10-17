2ce3a1aa1cd3c9a85ef0d8e5386b798a9bc6abcf
hubteas <tykim888@naver.com>
2024-10-17 16:45:11 +0900

    20241017 1644 김태유 최종

--- publishing/js/planner/myplanDetail.js
+++ publishing/js/planner/myplanDetail.js
@@ -6,3 +6,29 @@ $(function () {
   $(function () {
   $("#footer").load("../main/footer.html");
   });
+
+
+  const btnSave = document.querySelector('.save-btn');
+  const btnCancle = document.querySelector('.cancel-btn');
+
+  btnSave.addEventListener('click', function () {
+
+    if (confirm('편집하시겠습니까?')) {
+  
+      window.location.href = '../myplan/myplan.html';
+    } 
+  
+  });
+  
+   
+
+  btnCancle.addEventListener('click', function () {
+
+  if (confirm('삭제하시겠습니까?')) {
+
+    window.location.href = '../mypage/mypage.html';
+  } 
+
+});
+
+ 
